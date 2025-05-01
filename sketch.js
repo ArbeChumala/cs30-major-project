adbc96c744cc0dc0237b30cd41a2207f11074323
ArbeChumala <arbe.chumala5407@gmail.com>
2025-05-01 13:45:46 -0600

    familiarizing with p5.party by creating a simple ball moving demo

--- sketch.js
+++ sketch.js
@@ -5,12 +5,28 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let globals = {
+  x: 0,
+  y: 0,
+};
+
+function preload(){
+  partyConnect("wss://demoserver.p5party.org", "sam_and_arbe");
+  globals = partyLoadShared("globals");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
+  fill(100, 100, 150);
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 50);
+  circle(globals.x, globals.y, 50);
+}
+
+function mousePressed(){
+  globals.x = mouseX;
+  globals.y = mouseY;
 }
