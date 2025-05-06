4db22effd5627fc0f947509610a15c4c9408f3d9
ArbeChumala <arbe.chumala5407@gmail.com>
2025-05-06 14:16:24 -0600

    started rudimentary graphics for penguin game

--- sketch.js
+++ sketch.js
@@ -5,6 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let boxLength;
+let windowResizeRatio;
+let penguinArray = [];
+let boundaries = {};
+
 let globals = {
   x: 0,
   y: 0,
@@ -17,17 +22,59 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
-  fill(100, 100, 255);
+  setupWindow();
+
+  for(let i = 0; i<8; i++){
+    let somePenguin;
+
+    if(i % 2=== 0){
+      somePenguin = new Penguin("black");
+    }
+    else{
+      somePenguin = new Penguin("blue");
+    }
+
+    penguinArray.push(somePenguin);
+  }
+}
+
+function setupWindow(){
+  boxLength = 600;
+  boundaries.left = (width-boxLength)/2;
+  boundaries.right = (width+boxLength)/2;
+  boundaries.top = (height-boxLength)/2;
+  boundaries.bottom = (height+boxLength)/2;
 }
 
 function draw() {
-  background(220);
-  circle(globals.x, globals.y, 50);
+  noStroke();
+  background(200);
+  fill(255);
+  rect(boundaries.left, boundaries.top, boxLength, boxLength);
+  for(let penguin of penguinArray){
+    penguin.display();
+  }
+
 }
 
-function mousePressed(){
-  globals.x = mouseX;
-  globals.y = mouseY;
+// function mousePressed(){
+//   globals.x = mouseX;
+//   globals.y = mouseY;
+// }
+
+class Penguin{
+  constructor(colour){
+    this.x = random(boundaries.left, boundaries.right);
+    //add a check to make sure of no overlap
+    this.y = random(boundaries.top, boundaries.bottom);
+    this.colour = colour;
+    this.radius = 20;
+  }
+
+  display(){
+    noStroke();
+    fill(this.colour);
+    circle(this.x, this.y, this.radius*2);
+  }
 }
 
