304fb3e8f736b5116bb81a59db2c8f7f21ed10c8
ArbeChumala <arbe.chumala5407@gmail.com>
2025-05-08 12:16:20 -0600

    learning how to use matter.js

--- demos/sketch.js
+++ demos/sketch.js
@@ -6,16 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 // module aliases
-
-const {Engine, World, Bodies, Composite} = Matter;
-// let Engine = Matter.Engine,
-//   Render = Matter.Render,
-//   Runner = Matter.Runner,
-//   Bodies = Matter.Bodies,
-//   Composite = Matter.Composite;
-
+let Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
+  
 // create an engine
 let engine = Engine.create();
+let world = engine.world;
+
 
 // create two boxes and a ground
 let boxA = Bodies.rectangle(400, 200, 80, 80);
@@ -23,21 +23,43 @@ let boxB = Bodies.rectangle(450, 50, 80, 80);
 let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, ground);
 
-// run the renderer
-Render.run(render);
-
-// create runner
 let runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
 
-function setup() {
+function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
+function draw(){
   background(220);
+  
+}
+
+class Box{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.w = 20;
+    this.h = 20;
+
+    this.body = Bodies.rectangle(this.x, this.y, this.w, this.h);
+    Composite.add(world, this.body);
+  }
+
+  show(){
+    let pos = this.body.position;
+    let angle = this.body.angle;
+
+    push();
+
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+
+    pop();
+  }
 }
