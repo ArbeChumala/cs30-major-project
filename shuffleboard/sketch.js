195a24dc12711e30e87aa98b69f8c4f29eb277b1
samIam1507 <samuelwardell07@gmail.com>
2025-05-22 12:15:12 -0600

    working on creating pool setup

--- shuffleboard/sketch.js
+++ shuffleboard/sketch.js
@@ -19,7 +19,7 @@ Runner.run(runner, engine);
 
 let radius = 20;
 let balls = [];
-let coloursList = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "black"];
+let coloursList = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "lightgreen", "lightblue"];
 
 class Shuffleboard {
   constructor(x, y, team) {
@@ -79,16 +79,38 @@ function setup() {
   let isStriped = true;
   let x = 2 * width / 3;
   let y = height / 2;
-  for (let i = 14; 1 >= 0; i --) {
-    let aBall = new Ball(x, y, coloursList[Math.floor(i / 2)], isStriped);
-    balls.push(aBall);
+  let counter = [-1];
+  for (let n = 0; n < 5; n ++) {
+    let lastCounter = counter.length - 1;
+    counter.push(counter[lastCounter] + 1);
+    lastCounter ++;
+    if (counter[lastCounter] === 0) {
+      counter.splice(0, 1);
+    }
+    console.log(counter);
+    let aBall;
+    for (let number of counter) {
+      console.log(number);
+      let yModifier = number - lastCounter / 2;
+      if (lastCounter === 2 && number === 1) {
+        aBall = new Ball(x + lastCounter * 40 * 2**(1/2), y + yModifier * 40 * 2**(1/2), "black", false, true);
+      }
+      else {
+        aBall = new Ball(x + lastCounter * 40 * 2**(1/2), y + yModifier * 40 * 2**(1/2), coloursList[coloursList.length - 1], isStriped, false);
+        isStriped = !isStriped;
+      }
+      balls.push(aBall);
+      if (isStriped) {
+        coloursList.pop();
+      }
+    }
   }
 }
 
 function draw() {
   background(220);
-  for (let aBall of balls) {
-    aBall.show();
+  for (let ball of balls) {
+    ball.show();
   }
 }
 
