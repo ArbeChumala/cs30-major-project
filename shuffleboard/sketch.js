efa049f282c9b65b3df176e882ebaa6ab8e24ec1
samIam1507 <samuelwardell07@gmail.com>
2025-05-24 17:26:42 -0600

    working on cue striking cue ball

--- shuffleboard/sketch.js
+++ shuffleboard/sketch.js
@@ -20,25 +20,9 @@ Runner.run(runner, engine);
 let radius = 20;
 let balls = [];
 let coloursList = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "lightgreen", "lightblue"];
-
-class Shuffleboard {
-  constructor(x, y, team) {
-    this.x = x;
-    this.y = y;
-    this.diameter = 50;
-    this.team = team;
-    this.options = {
-      density: 0,
-    };
-
-    this.body = Bodies.circle(this.x, this.y, this.diameter, this.options);
-    Composite.add(world, this.body);
-  }
-
-  show() {
-
-  }
-}
+let ballsMoving = false;
+let isDrawnBack = false;
+let cueSpeedFactor = 20;
 
 
 
@@ -75,6 +59,7 @@ function setup() {
         coloursList.pop();
       }
     }
+    cue = new Cue(width / 3, height / 2);
   }
   aBall = new Ball(width / 3, height / 2, "white", false, false, true);
   balls.push(aBall);
@@ -85,24 +70,27 @@ function draw() {
   for (let ball of balls) {
     ball.show();
   }
+  cue.update();
+  cue.show();
 }
 
-function mousePressed() {
-  let aBall = new Ball(mouseX, mouseY, "red", true);
-  balls.push(aBall);
-}
+// function mousePressed() {
+//   let aBall = new Ball(mouseX, mouseY, "red", true);
+//   balls.push(aBall);
+// }
 
 
 // Classes
 // ---------------------------------------------------------------------------------------------------------
 
 class Ball {
-  constructor(x, y, colour, striped, eightBall) {
+  constructor(x, y, colour, striped, eightBall, cueBall) {
     this.x = x;
     this.y = y;
     this.colour = colour;
     this.striped = striped;
     this.eightBall = eightBall;
+    this.cueBall = cueBall;
 
     let options = {
       restitution: 0.1,
@@ -131,22 +119,98 @@ class Ball {
   update() {
     
   }
+
+  strikeCueBall(dx, dy) {
+    for (let ball of balls) {
+      if (this.cueBall) {
+        this.body.velocity(dx, dy);
+      }
+    }
+  }
 }
 
 class Cue {
   constructor(ballX, ballY) {
-    this.strikeX = ballX;
-    this.strikeX = ballY;
+    this.ballX = ballX;
+    this.ballY = ballY;
 
-    this.x = ballX - 100;
+    this.x = ballX - 100 - radius;
     this.y = ballY;
+    this.dx = 0;
+    this.dy = 0;
+
+    this.strikeX = ballX;
+    this.strikeY = ballY;
+
+    this.distanceX = 100;
+    this.distanceY = 0;
+
+    this.ratio;
+    this.distance;
+    this.strikeDistance;
+    this.strikeRatio;
+
+    this.isDrawnBack = false;
   };
 
   update() {
-    if (mouseIsPressed && mouseX < this.x + 25 && mouseX > this.x - 25 && mouseY > this.y - 25 && mouseY < this.y + 25) {
+    this.distance = dist(this.x, this.y, this.ballX, this.ballY);
+    this.strikeDistance = dist(this.strikeX, this.strikeY, this.ballX, this.ballY);
+    
+    if (this.movingIn) {
+      this.x -= this.strikeRatio * this.distanceX / cueSpeedFactor;
+      this.y -= this.strikeRatio * this.distanceY / cueSpeedFactor;
+
+      this.strikeX -= this.strikeRatio * this.distanceX / cueSpeedFactor;
+      this.strikeY -= this.strikeRatio * this.distanceY / cueSpeedFactor;
+
+      if (this.distance < 100 + radius) {
+        console.log("baddabing");
+        this.movingIn = false;
+      }
+    }
+    else if (mouseIsPressed && mouseX < this.x + 25 && mouseX > this.x - 25 && mouseY > this.y - 25 && mouseY < this.y + 25 &&this.distance >= 100) {
+      this.ratio = 100 /this.distance;
+      this.strikeRatio = 100 / this.strikeDistance;
+      
       this.x = mouseX;
       this.y = mouseY;
+
+      this.distanceX = mouseX - this.ballX;
+      this.distanceY = mouseY - this.ballY;
+
+      this.strikeX = this.x - this.ratio * this.distanceX;
+      this.strikeY = this.y - this.ratio * this.distanceY;
+
+      this.isDrawnBack = true;
+      this.movingIn = false;
     }
+    else if (this.isDrawnBack &&this.distance <= 100 + radius) {
+      this.isDrawnBack = false;
+
+      this.x = this.ballX - 100 - radius;
+      this.y = this.ballY;
+      this.strikeX = this.ballX;
+      this.strikeY = this.ballY;
+    }
+    else if (this.isDrawnBack && !mouseIsPressed) {
+      this.dx ++;
+      this.dy ++;
+      console.log("hi");
+      this.movingIn = true;
+      this.isDrawnBack = false;
+
+      this.distanceX = this.x - this.ballX;
+      this.distanceY = this.y - this.ballY;
+
+      cueSpeedFactor = 2000 / this.strikeDistance;
+    }
+  }
+
+  show() {
+    fill("black");
+    stroke(4);
+    line(this.x, this.y, this.strikeX, this.strikeY);
   }
 
 }
