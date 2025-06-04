70a6d1b55abc3c8d71677900fda271c2cb05fa56
ArbeChumala <arbe.chumala5407@gmail.com>
2025-06-03 20:48:05 -0600

    filler grid is synced and player can make moves
    future bug fix: only host-side player can make moves, fix it so guest side can on their turn

--- filler/sketch.js
+++ filler/sketch.js
@@ -1,22 +1,26 @@
-let theGrid;
-let desiredColour = 3;
+let grid;
 let checkedSpaces = [];
 
-let colourSequence = "";
+let colourArray = ["#F54E29", "#F89012", "#F7B926", "#90BE6D", "#43AA8B", "#577590"];
+let shared;
 
-const SQUARE_DIMENSIONS = 100;
+const SQUARE_DIMENSIONS = 75;
 const GAME_WIDTH= 8;
 const GAME_HEIGHT = 7;
+
+const PLAYER_ONE = -1;
+const PLAYER_TWO = -2;
+
+let yourPlayer;
 
 function generateEmptyGrid(){
   let newGrid = [];
   for(let y = 0; y<GAME_HEIGHT; y++){
     newGrid.push([]);
     for(let x = 0; x<GAME_WIDTH; x++){
-      newGrid[y].push([]);
+      newGrid[y].push(0);
     }
   }
-  newGrid[GAME_HEIGHT-1][0] = "y";
   return newGrid;
 }
 
@@ -24,9 +28,24 @@ function generateColourSequence(){
   let theSequence = "";
 
   for(let i = 0; i<GAME_WIDTH*GAME_HEIGHT; i++){
-    theSequence += str(Math.round(random(6)));
+    theSequence += str(Math.round(random(colourArray.length-1)));
   }
   return theSequence;
+}
+
+function setupGrid(){
+  let myGrid = generateEmptyGrid();
+
+  for(let y = 0; y<GAME_HEIGHT; y++){
+    for(let x = 0; x<GAME_WIDTH; x++){
+      myGrid[y][x] = int(shared.colourSequence[GAME_WIDTH*y + x]);
+    }
+  }
+
+  myGrid[GAME_HEIGHT -1][0] = PLAYER_ONE;
+  myGrid[0][GAME_WIDTH-1] = PLAYER_TWO;
+
+  return myGrid;
 }
 
 function preload(){
@@ -36,56 +55,85 @@ function preload(){
     "main"
   );
   tempSequence = generateColourSequence();
-  colourSequence = partyLoadShared("shared", {colourSequence: tempSequence});
+  shared = partyLoadShared(
+    "shared", 
+    {
+      colourSequence: tempSequence,
+      desiredColour: 0,
+      currentPlayer: PLAYER_ONE,
+    }
+  );
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  yourPlayer = partyIsHost() ? PLAYER_ONE : PLAYER_TWO;
+  grid = setupGrid();
+  partySubscribe("play", playerMoves);
 }
 
-// function draw(){
-//   for(let iy = 0; iy<GAME_HEIGHT; iy++){
-//     for(let ix = 0; ix<GAME_WIDTH; ix++){
-//       let x = (width - SQUARE_DIMENSIONS*GAME_WIDTH)/2 + SQUARE_DIMENSIONS*ix;
-//       let y = (height - SQUARE_DIMENSIONS*GAME_HEIGHT)/2 + SQUARE_DIMENSIONS*iy;
+function draw(){
+  for(let iy = 0; iy<GAME_HEIGHT; iy++){
+    for(let ix = 0; ix<GAME_WIDTH; ix++){
+      let x = (width - SQUARE_DIMENSIONS*GAME_WIDTH)/2 + SQUARE_DIMENSIONS*ix;
+      let y = (height - SQUARE_DIMENSIONS*GAME_HEIGHT)/2 + SQUARE_DIMENSIONS*iy;
 
-//       if(theGrid[iy][ix] !== "y"){
-//         fill(255/6*theGrid[iy][ix]);
-//       }
-//       else{
-//         fill("red");
-//       }
-//       square(x, y, SQUARE_DIMENSIONS);
-//     }
-//   }
-// }
+      if(grid[iy][ix] === PLAYER_ONE){
+        fill("black");
+      }
+      else if(grid[iy][ix] === PLAYER_TWO){
+        fill("white");
+      }
+      else{
+        fill(colourArray[grid[iy][ix]]);
+      }
 
-// function mouseClicked(){
-//   playerMoves();
-// }
+      square(x, y, SQUARE_DIMENSIONS);
+    }
+  }
+}
 
-// function playerMoves(){
-//   changeBoxes(0, GAME_HEIGHT-1);
-//   checkedSpaces = [];
-// }
+function keyPressed(){
+  if(shared.currentPlayer === yourPlayer){
+    partyEmit("play");
+    shared.desiredColour = int(key);
+    playerMoves();
+  }
+}
 
-// function changeBoxes(x, y){
-//   if(x < GAME_WIDTH && x >=0 && y<GAME_HEIGHT && y>=0){
-//     if(theGrid[y][x] === "y" && !checkedSpaces.includes(`${x}${y}`)){
-//       checkedSpaces.push(`${x}${y}`);
-//       changeNeighbours(x, y);
-//     }
-//     else if (theGrid[y][x] === desiredColour){
-//       checkedSpaces.push(`${x}${y}`);
-//       theGrid[y][x] = "y";
-//       changeNeighbours(x, y);
-//     }
-//   }
-// }
+function toggleCurrentPlayer(){
+  shared.currentPlayer = shared.currentPlayer === PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE;
+}
 
-// function changeNeighbours(x, y){
-//   changeBoxes(x, y+1);
-//   changeBoxes(x, y-1);
-//   changeBoxes(x+1, y);
-//   changeBoxes(x-1, y);
-// }
+function playerMoves(){
+  if(shared.currentPlayer === PLAYER_ONE){
+    changeBoxes(0, GAME_HEIGHT-1);
+    checkedSpaces = [];
+  }
+  else{
+    changeBoxes(GAME_WIDTH-1, 0);
+    checkedSpaces = [];
+  }
+  toggleCurrentPlayer();
+}
+
+function changeBoxes(x, y){
+  if(x < GAME_WIDTH && x >=0 && y<GAME_HEIGHT && y>=0){
+    if(grid[y][x] === shared.currentPlayer && !checkedSpaces.includes(`${x}${y}`)){
+      checkedSpaces.push(`${x}${y}`);
+      changeNeighbours(x, y);
+    }
+    else if (grid[y][x] === shared.desiredColour){
+      checkedSpaces.push(`${x}${y}`);
+      grid[y][x] = shared.currentPlayer;
+      changeNeighbours(x, y);
+    }
+  }
+}
+
+function changeNeighbours(x, y){
+  changeBoxes(x, y+1);
+  changeBoxes(x, y-1);
+  changeBoxes(x+1, y);
+  changeBoxes(x-1, y);
+}
