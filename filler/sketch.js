aae7302e825f06c251c72bc41359f0844e79d4b5
ArbeChumala <arbe.chumala5407@gmail.com>
2025-06-07 23:01:56 -0600

    changed graphics on filler and added score counters

--- filler/sketch.js
+++ filler/sketch.js
@@ -2,23 +2,38 @@ let myRoom = undefined;
 let userInput;
 
 let poppins;
+let frameImg;
+let frameShadowImg;
+let squareShadowImg;
 
 let grid;
 let checkedSpaces = [];
 
-let colourArray = ["#F54E29", "#F89012", "#F7B926", "#90BE6D", "#43AA8B", "#577590"];
+let colourArray = ["#FF50A4", "#FFAE00", "#FFEA00", "#00FFBB", "#00AEFF", "#984AFF"];
 let shared;
 
 let desiredColour;
 
-const SQUARE_DIMENSIONS = 75;
+let buttons = [];
+let scoreDisplayers = [];
+
+let playerOneColour;
+let playerTwoColour;
+
+const SQUARE_DIMENSIONS = 60;
 const GAME_WIDTH= 8;
 const GAME_HEIGHT = 7;
+const BUTTON_WIDTH = 60;
+const BUTTON_GAP = 20;
 
 const PLAYER_ONE = -1;
 const PLAYER_TWO = -2;
 
 let yourPlayer;
+
+const PIXEL_RATIO = 8*SQUARE_DIMENSIONS/151;
+const SHADOW_OFFSET = 20;
+const SCORE_BOX_WIDTH = 80;
 
 //-----------------------------------------------------------------------------------------------
 // automatic and player-input functions
@@ -26,14 +41,19 @@ let yourPlayer;
 
 function preload(){
   poppins = loadFont("assets/fonts/bold-poppins.ttf");
+  frameImg = loadImage("assets/images/frame.png");
+  frameShadowImg = loadImage("assets/images/outer-shadow.png");
+  squareShadowImg = loadImage("assets/images/square-shadow.png");
 }
 
 function setup(){
   noLoop();
   createCanvas(windowWidth, windowHeight);
+  noSmooth();
+  imageMode(CENTER);
   userInput = createInput('main');
   userInput.center();
-  background(120);
+  background("#948d8a");
   textSize(100);
   textAlign(CENTER);
   textFont(poppins);
@@ -43,36 +63,23 @@ function setup(){
 
 function draw(){
   if(myRoom){
-    background(120);
-    for(let iy = 0; iy<GAME_HEIGHT; iy++){
-      for(let ix = 0; ix<GAME_WIDTH; ix++){
-        let x = (width - SQUARE_DIMENSIONS*GAME_WIDTH)/2 + SQUARE_DIMENSIONS*ix;
-        let y = (height - SQUARE_DIMENSIONS*GAME_HEIGHT)/2 + SQUARE_DIMENSIONS*iy;
-  
-        if(grid[iy][ix] === PLAYER_ONE){
-          fill("black");
-        }
-        else if(grid[iy][ix] === PLAYER_TWO){
-          fill("white");
-        }
-        else{
-          fill(colourArray[grid[iy][ix]]);
-        }
-        noStroke();
-        square(x, y, SQUARE_DIMENSIONS);
-      }
-    }
+    background("#948d8a");
+    displayTiles();
+    displayFrame();
+    displayButtons();
+    displayScore();
   }
 }
 
 function keyPressed(){
-  if(myRoom && shared.currentPlayer === yourPlayer){
-    theColour = int(key);
-    partyEmit("play", {colour: theColour});
-  }
-  else if (key === "Enter" && !myRoom){
+  if (key === "Enter" && !myRoom){
     startParty();
-    
+  }
+}
+
+function mousePressed(){
+  for(let button of buttons){
+    button.checkIfPressed();
   }
 }
 
@@ -103,8 +110,17 @@ function startParty(){
 }
 
 function setupGame(){
+  for(let i = 0; i<colourArray.length; i++){
+    let someButton = new Button(height/2 + 300, i);
+    buttons.push(someButton);
+  }
+  for(let i = 0; i<2; i++){
+    let someDisplayer = new ScoreDisplayer(-(i+1));
+    scoreDisplayers.push(someDisplayer);
+  }
   yourPlayer = partyIsHost() ? PLAYER_ONE : PLAYER_TWO;
   grid = setupGrid(shared.colourSequence, true);
+
   removeElements();
   loop();
 }
@@ -116,13 +132,20 @@ function toggleCurrentPlayer(){
 function playerMoves(object){
   desiredColour = object.colour;
   if(shared.currentPlayer === PLAYER_ONE){
+    playerOneColour = desiredColour;
     changeBoxes(0, GAME_HEIGHT-1);
     checkedSpaces = [];
   }
   else{
+    playerTwoColour = desiredColour;
     changeBoxes(GAME_WIDTH-1, 0);
     checkedSpaces = [];
   }
+
+  for(let displayer of scoreDisplayers){
+    displayer.updateScore();
+  }
+  
   toggleCurrentPlayer();
 }
 
@@ -168,56 +191,15 @@ function generateColourSequence(){
     let isWarmColoured = (pretendX + pretendY)%2 === 0;
 
     if(isWarmColoured){
-      theSequence += str(Math.round(random(0,2.5)));
+      theSequence += str(Math.floor(random(0,3)));
     }
     else{
-      theSequence += str(Math.round(random(2.5,5)));
+      theSequence += str(Math.floor(random(3,6)));
     }
   }
 
   return theSequence;
 }
-
-// function demoTheSequence(theSequence){
-//   testingGrid = setupGrid(theSequence, false);
-//   newSequence = "";
-  
-//   for(let y = 0; y<GAME_HEIGHT; y++){
-//     for(let x = 0; x<GAME_WIDTH; x++){
-//       if(checkForTwins(testingGrid, x, y)){
-//         testingGrid[y][x] = colourArray.length-1;
-//         newSequence += str(colourArray.length-1);
-//       }
-//       else{
-//         newSequence += str(testingGrid[y][x]);
-//       }
-//     }
-//   }
-
-//   return newSequence;
-// }
-
-// function checkForTwins(myGrid, x, y){
-//   let matchesFound = 0;
-
-//   changes = [
-//     [x, y+1],
-//     [x, y-1],
-//     [x+1, y],
-//     [x-1, y]
-//   ];
-
-//   for(let change of changes){
-//     if( change[1] >=0 && change[1] < GAME_HEIGHT &&
-//         change[0] >=0 && change[0] < GAME_HEIGHT &&
-//         myGrid[change[1]][change[0]] !== colourArray.length-1 &&
-//         myGrid[y][x] === myGrid[change[1]][change[0]]){
-//       matchesFound ++;
-//     }
-//   }
-
-//   return matchesFound;
-// }
 
 function setupGrid(theSequence, isForReal){
   let myGrid = generateEmptyGrid();
@@ -229,9 +211,188 @@ function setupGrid(theSequence, isForReal){
   }
 
   if(isForReal){
+    playerOneColour = myGrid[GAME_HEIGHT -1][0];
+    playerTwoColour = myGrid[0][GAME_WIDTH-1];
+
     myGrid[GAME_HEIGHT -1][0] = PLAYER_ONE;
     myGrid[0][GAME_WIDTH-1] = PLAYER_TWO;
   }
 
   return myGrid;
+}
+
+function displayTiles(){
+  for(let iy = 0; iy<GAME_HEIGHT; iy++){
+    for(let ix = 0; ix<GAME_WIDTH; ix++){
+      let x = (width - SQUARE_DIMENSIONS*GAME_WIDTH)/2 + SQUARE_DIMENSIONS*ix;
+      let y = (height - SQUARE_DIMENSIONS*GAME_HEIGHT)/2 + SQUARE_DIMENSIONS*iy;
+
+      if(grid[iy][ix] === PLAYER_ONE){
+        if(str(playerOneColour) === playerOneColour){
+          fill(playerOneColour);
+        }
+        else{
+          fill(colourArray[playerOneColour]);
+        }
+      }
+      else if(grid[iy][ix] === PLAYER_TWO){
+        if(str(playerTwoColour) === playerTwoColour){
+          fill(playerTwoColour);
+        }
+        else{
+          fill(colourArray[playerTwoColour]);
+        }
+      }
+      else{
+        fill(colourArray[grid[iy][ix]]);
+      }
+      noStroke();
+      rectMode(CORNER);
+      square(x, y, SQUARE_DIMENSIONS);
+    }
+  }
+}
+
+function displayFrame(){
+  imageMode(CENTER);
+  image(frameShadowImg, width/2 - SHADOW_OFFSET, height/2 + SHADOW_OFFSET, frameShadowImg.width*PIXEL_RATIO, frameShadowImg.height*PIXEL_RATIO);
+  image(frameShadowImg, width/2 - 0.5*SHADOW_OFFSET, height/2 + 0.5*SHADOW_OFFSET, frameShadowImg.width*PIXEL_RATIO, frameShadowImg.height*PIXEL_RATIO);
+  image(frameImg, width/2, height/2, frameImg.width*PIXEL_RATIO, frameImg.height*PIXEL_RATIO);
+}
+
+function displayButtons(){
+  for(let button of buttons){
+    button.update();
+    button.show();
+  }
+}
+
+function displayScore(){
+  for(let displayer of scoreDisplayers){
+    displayer.update();
+    displayer.show();
+  }
+}
+
+//-----------------------------------------------------------------------------------------------
+//classes
+//-----------------------------------------------------------------------------------------------
+class Button{
+  constructor(y, colourIndex){
+    this.y = y;
+    this.width = BUTTON_WIDTH;
+    this.colourIndex = colourIndex;
+    this.isSelectable;
+    this.gradientTime = false;
+  }
+
+  show(){
+    this.x = width/2-(2.5*BUTTON_WIDTH + 2.5*BUTTON_GAP) + this.colourIndex*(BUTTON_WIDTH+BUTTON_GAP);
+
+    fill(colourArray[this.colourIndex]);
+    stroke("#5c5550");
+    strokeWeight(4);
+    rectMode(CENTER);
+    imageMode(CENTER);
+    image(squareShadowImg, this.x - 0.5*SHADOW_OFFSET, this.y + 0.5*SHADOW_OFFSET, this.width, this.width);
+    image(squareShadowImg, this.x - 0.25*SHADOW_OFFSET, this.y + 0.25*SHADOW_OFFSET, this.width, this.width);
+    square(this.x, this.y, this.width);
+
+    if(!this.isSelectable){
+      fill(10, 10, 10, 100);
+      square(this.x, this.y, this.width);
+    }
+
+    noStroke();
+  }
+
+  update(){
+    this.isSelectable = this.colourIndex !== playerOneColour && this.colourIndex !== playerTwoColour;
+
+    if(this.isSelectable){
+      if(this.gradientTime){
+        this.width -= 0.5;
+        this.gradientTime = this.width < BUTTON_WIDTH*0.95;
+      }
+      else{
+        this.width =  BUTTON_WIDTH;
+      }
+    }
+    else{
+      if(this.gradientTime){
+        this.width += 0.5;
+        this.gradientTime = this.width < BUTTON_WIDTH*0.75;
+      }
+      else{
+        this.width = 0.7*BUTTON_WIDTH;
+      }
+    }
+  }
+
+  checkIfPressed(){
+    if(this.isSelectable && 
+        shared.currentPlayer === yourPlayer &&
+        Math.abs(this.x - mouseX) < BUTTON_WIDTH/2 && 
+        Math.abs(this.y - mouseY) < BUTTON_WIDTH/2){
+
+      partyEmit("play", {colour: this.colourIndex});
+      this.gradientTime = true;
+    }
+  }
+}
+
+class ScoreDisplayer{
+  constructor(player){
+    this.player = player;
+    this.score = 1;
+
+    if(this.player === PLAYER_ONE){
+      this.x = width/2 - 350;
+    }
+    else{
+      this.x = width/2 + 350;
+    }
+    this.y = height/2;
+  }
+  
+  show(){
+    fill(colourArray[this.colour]);
+    stroke("#5c5550");
+    strokeWeight(4);
+    rectMode(CENTER);
+    square(this.x, this.y, BUTTON_WIDTH);
+    noStroke();
+
+    textSize(40);
+    text(this.score, this.x, this.y + 100);
+  }
+
+  update(){
+    this.updateColour();
+    this.updateSize();
+  }
+
+  updateColour(){
+    this.colour = this.player === PLAYER_ONE ? playerOneColour : playerTwoColour;
+  }
+
+  updateSize(){
+    this.size = shared.currentPlayer === this.player ? SCORE_BOX_WIDTH : 0.5*SCORE_BOX_WIDTH;
+  }
+
+  updateScore(){
+    if(shared.currentPlayer === this.player){
+      let score = 0;
+  
+      for(let y = 0; y<GAME_HEIGHT; y++){
+        for(let x = 0; x<GAME_WIDTH; x++){
+          if(grid[y][x] === this.player){
+            score ++;
+          }
+        }
+      }
+  
+      this.score = score;
+    }
+  }
 }
